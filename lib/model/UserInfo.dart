e5c7a8c1ea9e3f3f3b8d89aea13a3ec37d36c636
NITISH KUMAR SINGH <nitishk72@gmail.com>
2018-08-09 14:22:19 +0530

    adding new json user_info

---
+++ lib/model/UserInfo.dart
@@ -0,0 +1,22 @@
+class UserInfo {
+  final int index;
+  final String name;
+  final String address;
+  final String gender;
+  final String email;
+  final String phone;
+  final String company;
+  final int year;
+  final String month;
+
+  UserInfo(
+      {this.index,
+      this.name,
+      this.address,
+      this.gender,
+      this.email,
+      this.phone,
+      this.company,
+      this.year,
+      this.month});
+}
--- lib/screens/List/List01.dart
+++ lib/screens/List/List01.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_examples/model/UserInfo.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class List01 extends StatefulWidget {
   @override
@@ -6,20 +9,54 @@ class List01 extends StatefulWidget {
 }
 
 class _List01State extends State<List01> {
+  final String URL =
+      'https://raw.githubusercontent.com/FlutterExamples/FlutterExamples/master/lib/data/user_info.json';
+  bool isData = false;
+  List<UserInfo> data = [];
+  GlobalKey<ScaffoldState> _key = new GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    data = [];
+    var Response = await http.get(
+      "https://api.github.com/users/nitishk72",
+      headers: {"Accept": "application/json"},
+    );
+    if (Response.statusCode == 200) {
+      String responseBody = Response.body;
+      var users = json.decode(responseBody);
+      for (var user in users) {
+        print(user);
+      }
+      isData = true;
+      setState(() {});
+    } else {
+      _key.currentState
+          .showSnackBar(SnackBar(content: new Text('Something went wrong !')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: Text('Stripped Backgroud'),
-        ),
-        body: ListView.builder(
-            itemCount: 20, itemBuilder: (_, index) =>ListUI(index)),);
+      key: _key,
+      appBar: new AppBar(
+        title: Text('Stripped Backgroud'),
+      ),
+      body: ListView.builder(
+          itemCount: 20, itemBuilder: (_, index) => ListUI(index)),
+    );
   }
-  Widget ListUI(int index){
+
+  Widget ListUI(int index) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 10.0),
-      color: (index+1)%2==0?Colors.blueGrey:Colors.white,
+      padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+      color: (index + 1) % 2 == 0 ? Colors.blueGrey : Colors.white,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
